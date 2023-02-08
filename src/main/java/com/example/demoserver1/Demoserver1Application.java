b7f839e1bcbda29dabe3e45d84f26e62872b56b2
epodoprigora <egor.podoprigora@gmail.com>
2023-02-08 23:04:56 +0800

    Initial commit

---
+++ src/main/java/com/example/demoserver1/Demoserver1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoserver1Application.class, args);
+	}
+
+}
